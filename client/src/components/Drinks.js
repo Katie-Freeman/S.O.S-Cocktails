110a6054ac79d53083842cf87a27ae538d009836
Katie Freeman <katiefreeman@carltons-mbp.lan>
2022-03-16 09:56:50 -0400

    model associations

--- client/src/components/Drinks.js
+++ client/src/components/Drinks.js
@@ -1,39 +1,41 @@
-import React, { useState } from "react"
-import styles from './drinks.module.css'
+import React, { useState } from "react";
+import IngredientSearch from "./IngredientSearch";
+import styles from "./drinks.module.css";
 
 function Drinks() {
-    const [drink, setDrink] =useState({})
+  const [drink, setDrink] = useState({});
 
-    const handleTextChange = (e) => {
+  const handleTextChange = (e) => {
     setDrink({
-        ...drink,
-        [e.target.name]: e.target.value,
-     });
-    };
+      ...drink,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSavedDrink = () => {
-        fetch("http://localhost:8080/drinks", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(drink),
-        })
-        .then((response) => response.json())
-        .then((result)=> {
-            console.log(result)
-        })
-    }
-    
-    return (
-      <div className={styles.drinkContainer}>
-        <h1>Add Drinks</h1>
-        <input type="text" onChange={handleTextChange} name="name" />
-        <input type="text" onChange={handleTextChange} name="imgUrl" />
-        <textarea type="text" onChange={handleTextChange} name="recipe" />
-        <button onClick={handleSavedDrink}>Submit</button>
-      </div>
-    );
+  const handleSavedDrink = () => {
+    fetch("http://localhost:8080/drinks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(drink),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
+  return (
+    <div className={styles.drinkContainer}>
+      <IngredientSearch />
+      <h1>Drink Recomendations</h1>
+      <input type="text" onChange={handleTextChange} name="name" />
+      <input type="text" onChange={handleTextChange} name="imgUrl" />
+      <textarea type="text" onChange={handleTextChange} name="recipe" />
+      <button onClick={handleSavedDrink}>Submit</button>
+    </div>
+  );
 }
 
-export default Drinks
+export default Drinks;
---
+++ client/src/components/IngredientSearch.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import styles from "./ingredientSearch.module.css";
+import OwnedIngredients from "./OwnedIngredients";
+
+const IngredientSearch = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getUserIngredients = () => {
+    setIsLoading(true);
+    const id = localStorage.getItem("userId");
+    fetch(`http://localhost:8080/users/${id}/ingredients`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((ownedIngredients) => {
+        setIsLoading(false);
+        setUserIngredients(ownedIngredients);
+      });
+  };
+  const fuzzySearch = () => {
+    fetch("http://localhost:8080/ingredients/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: search }),
+    })
+      .then((response) => response.json())
+      .then((res) => setResults(res.matches));
+  };
+
+  useEffect(() => {
+    getUserIngredients();
+  }, []);
+
+  return (
+    <div className={styles.searchContainer}>
+      <OwnedIngredients ingredients={userIngredients} isLoading={isLoading} />
+      <input
+        className={styles.searchInput}
+        type="text"
+        value={search}
+        placeholder="Enter an ingredient"
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyUp={() => setTimeout(fuzzySearch(), 2000)}
+      />
+      <div className={styles.ingredientGrid}>
+        {results.map((result) => (
+          <IngredientCard
+            ingredient={result}
+            userIngredients={userIngredients}
+            setUserIngredients={setUserIngredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const IngredientCard = ({
+  ingredient,
+  userIngredients,
+  setUserIngredients,
+}) => {
+  const ownedIngredientIds = userIngredients.map((ingredient) => ingredient.id);
+  const isOwned = ownedIngredientIds.includes(ingredient.id);
+
+  const selectIngredient = (ingredientId) => {
+    const userId = localStorage.getItem("userId");
+    fetch(`http://localhost:8080/users/${userId}/add-ingredients`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ingredientId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (!response.error) {
+          setUserIngredients(response.ingredients);
+        }
+      });
+  };
+
+  const deleteIngredient = (ingredientId) => {
+    const userId = localStorage.getItem("userId");
+    fetch(`http://localhost:8080/users/${userId}/ingredients`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ingredientId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (!response.error) {
+          setUserIngredients(response.ingredients);
+        }
+      });
+  };
+
+  useEffect(() => {}, [isOwned]);
+
+  return (
+    <div
+      className={
+        isOwned
+          ? `${styles.ingredientCard} ${styles.owned}`
+          : styles.ingredientCard
+      }
+      key={ingredient.id}
+    >
+      <span>{ingredient.name}</span>
+      <button
+        onClick={
+          isOwned
+            ? () => deleteIngredient(ingredient.id)
+            : () => selectIngredient(ingredient.id)
+        }
+      >
+        {isOwned ? "✖️" : "✔"}️
+      </button>
+    </div>
+  );
+};
+
+export default IngredientSearch;
