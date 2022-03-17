fa231b377f34ab0398cf2d3209841fb72cd73b1a
Katie Freeman <katiefreeman@carltons-mbp.lan>
2022-03-17 11:49:42 -0400

    deployment corrections

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicPath));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname,'../client/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', "index.html")))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', "static", "index.html")))
 }else{
   app.get('/', (res, req) => res.send('Please set to production'))
 }
